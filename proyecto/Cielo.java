1cfac5a345da6dec97a75ead0982139259657aff
Daniel <futbox10@gmail.com>
2015-04-28 13:35:04 -0500

    Se sincronizo el dia y la noche en la clase cielo

--- proyecto/Cielo.java
+++ proyecto/Cielo.java
@@ -11,6 +11,8 @@ public class Cielo extends ScrollActor
 {
     private int r, g, b;
     private int c;
+    private int v;
+    private boolean dia;
     private GreenfootImage sky = new GreenfootImage(800,400);
     private SimpleTimer timer = new SimpleTimer();
     /**
@@ -23,6 +25,8 @@ public class Cielo extends ScrollActor
         g = green;
         b = blue;
         c = 0;
+        v = 0;
+        dia = true;
         sky.setColor(new Color(r, g, b));
         sky.fillRect(0, 0, 800, 400);
         setImage(sky);
@@ -31,23 +35,68 @@ public class Cielo extends ScrollActor
     
     public void act()
     {
-        if(timer.millisElapsed()>500)
+        if(timer.millisElapsed()>1020)
         {
-            incCont();
-            cambiaColor(c);
+            if(c < g && dia == true)
+            {
+                incCont(1);
+                if (c % 5 == 0)
+                {
+                    incCont(0);
+                }
+            }
+            else
+            {
+                dia = false;
+                decCont(1);
+                if (c % 5 == 0)
+                {
+                    decCont(0);
+                }
+                if(c == 0)
+                {
+                    dia = true;
+                }
+            }
+            cambiaColor(c ,v);
+            //System.out.println("g: " + c + " b: " + v);
             timer.mark();
         }
     }
     
-    private void cambiaColor(int tiempo)
+    private void cambiaColor(int tiempo, int v)
     {
-        sky.setColor(new Color(r,g-tiempo,b));
+        sky.setColor(new Color(r,g - tiempo,b - v));
         sky.fillRect(0,0,800,600);
         setImage(sky);
     }
     
-    private void incCont()
+    private void incCont(int valor)
     {
-        c++;
+        if(valor == 1)
+        {
+            c++;
+        }
+        else
+        {
+            v++;
+        }
+    }
+    
+    private void decCont(int valor)
+    {
+        if(valor == 1)
+        {
+            c--;
+        }
+        else
+        {
+            v--;
+        }
+    }
+    
+    public boolean getDia()
+    {
+        return dia;
     }
 }
