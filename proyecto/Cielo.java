982ac4c6fe63300c1a90df07d8a74ee7ce80f5ed
Daniel <futbox10@gmail.com>
2015-04-28 12:04:30 -0500

    Se modifico la clase Cielo para que haga transicion entre dia y noche

--- proyecto/Cielo.java
+++ proyecto/Cielo.java
@@ -9,24 +9,45 @@ import java.awt.Color;
  */
 public class Cielo extends ScrollActor
 {
-     private GreenfootImage sky = new GreenfootImage(800,400);
+    private int r, g, b;
+    private int c;
+    private GreenfootImage sky = new GreenfootImage(800,400);
     private SimpleTimer timer = new SimpleTimer();
     /**
      * Act - do whatever the Cielo wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public Cielo(int r, int g, int b)
+    public Cielo(int red, int green, int blue)
     {
+        r = red;
+        g = green;
+        b = blue;
+        c = 0;
         sky.setColor(new Color(r, g, b));
         sky.fillRect(0, 0, 800, 400);
         setImage(sky);
         timer.mark();
     }
     
+    public void act()
+    {
+        if(timer.millisElapsed()>500)
+        {
+            incCont();
+            cambiaColor(c);
+            timer.mark();
+        }
+    }
+    
     private void cambiaColor(int tiempo)
     {
-        sky.setColor(new Color(0,148-tiempo,255));
+        sky.setColor(new Color(r,g-tiempo,b));
         sky.fillRect(0,0,800,600);
         setImage(sky);
-    }  
+    }
+    
+    private void incCont()
+    {
+        c++;
+    }
 }
