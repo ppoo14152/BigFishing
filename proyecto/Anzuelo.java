48f2f75fdeb9a02216afbecf327332b25e5a603d
Daniel <futbox10@gmail.com>
2015-04-29 17:50:14 -0500

    Clase Anzuelo finalizada

--- proyecto/Anzuelo.java
+++ proyecto/Anzuelo.java
@@ -16,6 +16,7 @@ public class Anzuelo extends ScrollActor
     private int move;
     private int posOriginalX, posOriginalY;
     private boolean exitePez;
+    private SimpleTimer timer = new SimpleTimer();
     /**
      * Move to face the mouse,
      * and listen to the up and down keys.
@@ -30,72 +31,61 @@ public class Anzuelo extends ScrollActor
         move = 3;
     }
     
-    public int globalX(){
+    public int globalX()
+    {
         return this.getGlobalX();
     }
     
-    public int globalY(){
+    public int globalY()
+    {
         return this.getGlobalY();
     }
     
     public void act()
     {
         getWorld().setCameraDirection(270);
-        
-        /*if(Greenfoot.isKeyDown("space"))
+    
+        if(Greenfoot.isKeyDown("w") && baja == true)
         {
-            if(baja == false)
-            {
-                System.out.println("Aqui");
+            //timer.mark();
+            //if(timer.millisElapsed() > 100)
+            //{
+                while(this.getGlobalY() != posOriginalY)
+                {
+                    //System.out.println("Aqui");
+                    //if((timer.millisElapsed() > 50))
+                    //{
+                        if(this.getGlobalY() > 900 && p != 0)
+                        {
+                            p--;
+                            getWorld().cambiaFondo(p);
+                        }
+                        getWorld().moveCamera(MOVE_AMOUNT);
+                        //baja = false;
+                      //  timer.mark();
+                    //}
+                }
+                baja = false;
+              //  timer.mark();
+            //}
+        }
+        
+        if(Greenfoot.isKeyDown("s") && baja == false)
+        {
+            //timer.mark();
+            //if(timer.millisElapsed() > 100)
+            //{
                 baja = true;
                 posOriginalX =this.getGlobalX();
                 posOriginalY =this.getGlobalY();
                 getWorld().moveCamera(-100);
                 limit = this.getX()-30;
-           }
-            if(this.getGlobalY() != posOriginalY)
-            {
-            while(this.getGlobalY() != posOriginalY)
-            {
-                if(this.getGlobalY() > 900 && p != 0)
-                {
-                    p--;
-                    getWorld().cambiaFondo(p);
-                }
-                getWorld().moveCamera(MOVE_AMOUNT);
-                baja = false;
-            }
-            }
-        }*/
-        
-        if(Greenfoot.isKeyDown("space") && baja == true)
-        {
-            if(this.getGlobalY() != posOriginalY)
-            {
-                while(this.getGlobalY() != posOriginalY)
-                {
-                    if(this.getGlobalY() > 900 && p != 0)
-                    {
-                        p--;
-                        getWorld().cambiaFondo(p);
-                    }
-                getWorld().moveCamera(MOVE_AMOUNT);
-                baja = false;
-            }
-            }
-        }
-        
-        if(Greenfoot.isKeyDown("space") && baja == false)
-        {
-            baja = true;
-            posOriginalX =this.getGlobalX();
-            posOriginalY =this.getGlobalY();
-                getWorld().moveCamera(-100);
-                limit = this.getX()-30;
- 
+                timer.mark();
+            //}
         }
         
         if (Greenfoot.isKeyDown("a") && (getX()>0)) {
+            
             if(baja == false)
             {
                 setLocation(getX() - 3, getY());
